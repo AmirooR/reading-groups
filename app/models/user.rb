f5471fe12250c6f6feb0f582b6f40eb9a10ef9df
a.rahimi <a.rahimi@idehno.com>
2014-05-26 20:42:23 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,14 @@ class User < ActiveRecord::Base
         end
 
 	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-	VALID_NAME_REGX = /\A\w+\Z/i
+	VALID_NAME_REGEX = /\A\w+\Z/i
 
 	before_create :create_remember_token
 
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }, confirmation: true
 	validates :email_confirmation, presence: true
-	validates :login, presence: true, format: {with: VALID_NAME_REGEX}, uniqueness: { case_sensitive: false }
+	validates :login, presence: true, format: {with: VALID_NAME_REGEX, message: "Only characters and digits"}, uniqueness: { case_sensitive: false }
 
 	validates :password, presence: true, length: {minimum: 8}
 	validates :password_confirmation, presence: true
