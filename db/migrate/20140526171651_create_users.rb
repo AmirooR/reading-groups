1706580b43a425ac89939fd31d169d3c15e520ad
a.rahimi <a.rahimi@idehno.com>
2014-05-26 17:23:38 +0000

    Initial commit, after generating users scaffold

---
+++ db/migrate/20140526171651_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :login
+      t.string :password_digest
+      t.string :remember_token
+      t.boolean :active, default: false
+
+      t.timestamps
+    end
+
+  end
+end
